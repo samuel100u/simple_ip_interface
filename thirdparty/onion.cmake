654495d715c32a7bbde1a6f90058a5fd8c701d32
hibati <qcap8591@gmail.com>
2018-05-11 02:41:08 +0000

    add thirdpary

---
+++ thirdparty/onion.cmake
@@ -0,0 +1,35 @@
+set(ONION_PREFIX onion04)
+
+ExternalProject_Add(
+  ${ONION_PREFIX}
+  GIT_REPOSITORY "https://github.com/davidmoreno/onion.git"
+  GIT_TAG "master"
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ""
+  BUILD_IN_SOURCE 0
+  SOURCE_DIR "${CMAKE_SOURCE_DIR}/thirdparty/onion"
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/${ONION_PREFIX} -DONION_EXAMPLES=false
+  TEST_COMMAND ""
+)
+
+
+
+set(ONION_DIR ${CMAKE_CURRENT_BINARY_DIR}/${ONION_PREFIX})
+
+
+include_directories("${ONION_DIR}/include")
+
+link_directories("${ONION_DIR}/lib")
+
+#message("${CMAKE_SHARED_LIBRARY_PREFIX}humblelogging${CMAKE_SHARED_LIBRARY_SUFFIX}")
+
+set(ONION_LIBS "*.a")
+
+
+
+
+message("${CMAKE_SOURCE_DIR}")
+
+message("${PROJECT_SOURCE_DIR}")
+
+message("${CMAKE_CURRENT_BINARY_DIR}")
